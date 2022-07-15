1a41be7b6f37f595db504bd28613fc7b62c3a44c
liangzaixian <liangzai@xian.com>
2022-07-15 14:28:21 +0800

    完成curd代码生成

--- mall-product/src/test/java/com/chillpt/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/chillpt/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,43 @@
 package com.chillpt.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.chillpt.mall.product.entity.BrandEntity;
+import com.chillpt.mall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+
+//		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setBrandId(1L);
+//		brandEntity.setDescript("靓仔先");
+
+//		brandEntity.setName("靓仔先");
+//		brandService.save(brandEntity);
+//		log.info("保存成功");
+
+//		brandService.updateById(brandEntity);
+
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		list.forEach((item)->{
+			System.out.println(item);
+		});
+
 	}
 
 }
